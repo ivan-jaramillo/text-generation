94c4d04643a877379d95e071b1fd93b1009db6fc
Ivan Jaramillo <ivandavilajaramillo@gmail.com>
2021-08-10 14:43:33 -0500

    Visually analyze dataset
    Nothing crazy here. Just looking at all the characters from the dataset.

--- text-generation/main.py
+++ text-generation/main.py
@@ -1,5 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+
+import numpy as np
+import os
+import time
+
+
 def main():
-    pass
+    text = open('data/the-great-gatsby.txt').read()
+    text += open('data/the-war-of-the-worlds.txt').read()
+    text += open('data/thus-spoke-zarathustra.txt').read()
+    text += open('data/the-importance-of-being-earnest.txt').read()
+    
+    # Get unique characters from the dataset.
+    vocab = sorted(set(text))
+
+    # Vectorize the text.
+    example_texts = ['abcdefg', 'xyz']
+    chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+    print(chars)
 
 
 if __name__ == '__main__':
