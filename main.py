d94047c0fef1bbd7f247e04ee4adeb14d99ac91d
Ivan Jaramillo <ivandavilajaramillo@gmail.com>
2022-06-01 20:02:59 -0500

    Create main.py
    This is where the main code will be.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Text Generation
+
+Generate text using a character-based recurrent neural network.
+"""
+
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == '__main__':
+    main()
